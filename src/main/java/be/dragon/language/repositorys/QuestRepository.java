fc5c1dc10ce9fa01756cd61bc2ce916849562191
benoit <benoit.goethals@gmail.com>
2017-11-28 23:02:07 +0100

    <plugin> 				<groupId>org.apache.maven.plugins</groupId> 				<artifactId>maven-jar-plugin</artifactId> 				<version>2.4</version> 				<configuration> 					<encoding>UTF-8</encoding> 				</configuration> 			</plugin>

--- src/main/java/be/dragon/language/repositorys/QuestRepository.java
+++ src/main/java/be/dragon/language/repositorys/QuestRepository.java
@@ -3,11 +3,13 @@ package be.dragon.language.repositorys;
 import be.dragon.language.model.Category;
 import be.dragon.language.model.Quest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Repository
+@Transactional(propagation = Propagation.SUPPORTS)
 public interface QuestRepository extends JpaRepository<Quest,Long> {
 
     /**
